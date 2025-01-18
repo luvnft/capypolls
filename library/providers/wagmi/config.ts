9b9bdbee0f41c1312f0d94e057d524602777faf2
The Wizard of Hahz <info@luvnft.com>
2025-01-18 08:39:34 -0500

    up

--- library/providers/wagmi/config.ts
+++ library/providers/wagmi/config.ts
@@ -1,19 +1,22 @@
 import { getDefaultConfig } from "connectkit";
 import { createConfig, http } from "wagmi";
-import { sepolia } from "wagmi/chains"; // Assuming Sepolia is available in wagmi (Base Sepolia is based on Sepolia)
+import { anvil, opBNBTestnet } from "wagmi/chains";
 
 const isDev = process.env.NODE_ENV === "development";
 
 export const config = createConfig(
   getDefaultConfig({
-    appName: "capypolls",
+    appName: "memepoll",
     walletConnectProjectId:
       process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
-    chains: [sepolia], // Use Sepolia (Base Sepolia Testnet) here
+    chains: [opBNBTestnet],
+    // chains: isDev ? [anvil] : [opBNBTestnet],
     multiInjectedProviderDiscovery: true,
     transports: {
-      [sepolia.id]: http(
-        "https://base-sepolia.g.alchemy.com/v2/DAfFtjudU6WUxNJuJ5RnM3gdKcx5hEHY" // Alchemy RPC URL for Base Sepolia
+      [anvil.id]: http(),
+
+      [opBNBTestnet.id]: http(
+        "https://opbnb-testnet.g.alchemy.com/v2/DAfFtjudU6WUxNJuJ5RnM3gdKcx5hEHY"
       ),
     },
   })
